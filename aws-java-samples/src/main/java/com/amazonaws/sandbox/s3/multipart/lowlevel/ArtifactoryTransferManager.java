ddafd0842353a65878144d81090c4de17e1d294f
Ualter Azambuja Jr. <ualter.azambuja1@allianz.com>
2020-06-11 11:57:01 +0200

    .

--- aws-java-samples/src/main/java/com/amazonaws/sandbox/s3/multipart/lowlevel/ArtifactoryTransferManager.java
+++ aws-java-samples/src/main/java/com/amazonaws/sandbox/s3/multipart/lowlevel/ArtifactoryTransferManager.java
@@ -33,8 +33,8 @@ public class ArtifactoryTransferManager {
         }
 		
 		if (origin.equals("NONE")) {
-			 System.out.println("Origin=" + origin + "  INVALID!";);
-             System.exit(1)
+			 System.out.println("Origin=" + origin + "  INVALID!");
+             System.exit(1);
 		}
 		
 		String bucketName = "emagin-delivery/general/artifactory";
